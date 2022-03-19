a4ee18ca9ce2d80bb4138d22d85c7c24d59592a3
JoshuaDeanHughes <62324107+JoshuaDeanHughes@users.noreply.github.com>
2022-03-18 19:36:51 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#docHide').click(function(){
+        $('#doc').toggle();
+    });
+    $('#webHide').click(function(){
+        $('#webpage').toggle();
+    });
+    $('#webHide2').click(function(){
+        $('#webpage2').toggle();
+    });
+});
